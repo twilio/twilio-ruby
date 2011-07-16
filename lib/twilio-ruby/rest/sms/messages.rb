1edd001891f23708a4ddaa9137b2200f5429effb
Andrew Benton <andrewmbenton@gmail.com>
2011-07-16 13:55:42 -0700

    fixed a horrible bug in sms messages that prevented #list from returning the actual list

--- lib/twilio-ruby/rest/sms/messages.rb
+++ lib/twilio-ruby/rest/sms/messages.rb
@@ -1,5 +1,11 @@
 module Twilio
   module REST
-    class Messages < ListResource; end
+    class Messages < ListResource
+      def initialize(uri, client)
+        super
+        # hard-code the json key since 'messages' doesn't exist in the response
+        @resource_name = 'sms_messages'
+      end
+    end
   end
 end
