047a1b6e5d4dd6cd5ae70f3f22d5a7466ca494c2
Andrew Benton <andrewmbenton@gmail.com>
2011-07-30 16:01:22 -0700

    new specs for Twilio Client Capability Token generation class

--- test/twilio_spec.rb
+++ test/twilio_spec.rb
@@ -208,3 +208,125 @@ describe Twilio::Util::RequestValidator do
     validator.validate(url, params, signature).should == true
   end
 end
+
+describe Twilio::Util::Capability do
+  before :each do
+    @capability = Twilio::Util::Capability.new 'myAccountSid', 'myAuthToken'
+  end
+
+  it 'should return a valid jwt when #generate is called' do
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].should_not be_nil
+    decoded['iss'].should_not be_nil
+    decoded['exp'].should_not be_nil
+  end
+
+  it 'should properly set the iss key in the payload' do
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['iss'].should == 'myAccountSid'
+  end
+
+  it 'should properly set the exp key based on the default hour ttl' do
+    seconds = Time.now.to_i
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['exp'].should == seconds + 3600
+  end
+
+  it 'should properly set the exp key based on the ttl passed to #generate' do
+    ttl = rand 10000
+    seconds = Time.now.to_i
+    token = @capability.generate ttl
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['exp'].should == seconds + ttl
+  end
+
+  it 'should generate a proper incoming client scope string' do
+    @capability.allow_client_incoming 'andrew'
+    expected_scope = 'scope:client:incoming?clientName=andrew'
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+
+  it 'should generate multiple proper incoming client scope strings' do
+    @capability.allow_client_incoming 'andrew'
+    @capability.allow_client_incoming 'bridget'
+    expected_scope_one = 'scope:client:incoming?clientName=andrew'
+    expected_scope_two = 'scope:client:incoming?clientName=bridget'
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope_one).should be_true
+    decoded['scope'].include?(expected_scope_two).should be_true
+  end
+
+  it 'should generate a proper outgoing client scope string' do
+    @capability.allow_client_outgoing 'myAppSid'
+    expected_scope = 'scope:client:outgoing?appSid=myAppSid'
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+
+  it 'should generate a proper outgoing client scope string with parameters' do
+    app_params_hash = {'key' => 'a value', :foo => 'bar/baz'}
+    @capability.allow_client_outgoing 'myAppSid', app_params_hash
+    app_params = @capability.instance_eval {url_encode(app_params_hash)}
+    params_hash = {'appSid' => 'myAppSid', 'appParams' => app_params}
+    params = @capability.instance_eval {url_encode(params_hash)}
+    expected_scope = "scope:client:outgoing?#{params}"
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+
+  it 'should generate a proper outgoing client scope string based on the ' +
+    'client name when calling #allow_client_incoming first' do
+    @capability.allow_client_incoming 'andrew'
+    @capability.allow_client_outgoing 'myAppSid'
+    expected_scope = 'scope:client:outgoing?clientName=andrew&appSid=myAppSid'
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+
+  it 'should generate a proper outgoing client scope string based on the ' +
+    'client name when calling #allow_client_incoming second' do
+    @capability.allow_client_outgoing 'myAppSid'
+    @capability.allow_client_incoming 'andrew'
+    expected_scope = 'scope:client:outgoing?clientName=andrew&appSid=myAppSid'
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+
+  it 'should generate a proper outgoing client scope string with parameters ' +
+    'and a client name when calling #allow_client_incoming first' do
+    @capability.allow_client_incoming 'andrew'
+    app_params_hash = {'key' => 'a value', :foo => 'bar/baz'}
+    @capability.allow_client_outgoing 'myAppSid', app_params_hash
+    app_params = @capability.instance_eval {url_encode(app_params_hash)}
+    params_hash = {'appSid' => 'myAppSid', 'appParams' => app_params, 'clientName' => 'andrew'}
+    params = @capability.instance_eval {url_encode(params_hash)}
+    expected_scope = "scope:client:outgoing?#{params}"
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+
+  it 'should generate a proper outgoing client scope string with parameters ' +
+    'and a client name when calling #allow_client_incoming second' do
+    app_params_hash = {'key' => 'a value', :foo => 'bar/baz'}
+    @capability.allow_client_outgoing 'myAppSid', app_params_hash
+    @capability.allow_client_incoming 'andrew'
+    app_params = @capability.instance_eval {url_encode(app_params_hash)}
+    params_hash = {'appSid' => 'myAppSid', 'appParams' => app_params, 'clientName' => 'andrew'}
+    params = @capability.instance_eval {url_encode(params_hash)}
+    expected_scope = "scope:client:outgoing?#{params}"
+    token = @capability.generate
+    decoded = JWT.decode token, 'myAuthToken'
+    decoded['scope'].include?(expected_scope).should be_true
+  end
+end
