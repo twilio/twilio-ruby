fce44fab9af3937eb9f6e4619a82946f43288731
Andrew Benton <andrewmbenton@gmail.com>
2011-08-26 11:25:18 -0700

    support for getting recording urls in different formats and streaming the binary data

--- lib/twilio-ruby/rest/recordings.rb
+++ lib/twilio-ruby/rest/recordings.rb
@@ -6,6 +6,26 @@ module Twilio
       def initialize(uri, client, params={})
         super uri, client, params
         resource :transcriptions
+        # grab a reference to the client's connection object for streaming
+        @connection = @client.instance_variable_get :@connection
+      end
+
+      def wav
+        scheme = @connection.use_ssl ? 'https' : 'http'
+        "#{scheme}://#{@connection.address}#{@uri}.wav"
+      end
+
+      def wav!(&block)
+        @connection.request_get @uri, &block
+      end
+
+      def mp3
+        scheme = @connection.use_ssl ? 'https' : 'http'
+        "#{scheme}://#{@connection.address}#{@uri}.mp3"
+      end
+
+      def mp3!(&block)
+        @connection.request_get "#{@uri}.mp3", &block
       end
     end
   end
