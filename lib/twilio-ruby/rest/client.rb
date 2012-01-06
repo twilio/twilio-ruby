682171cd5fcb0c9aecac386a6cf25155e1297dfb
Andrew Benton <andrewmbenton@gmail.com>
2012-01-06 00:04:00 -0800

    trying a different location for cacert

--- lib/twilio-ruby/rest/client.rb
+++ lib/twilio-ruby/rest/client.rb
@@ -208,7 +208,7 @@ module Twilio
           if config[:ssl_ca_file]
             @connection.ca_file = config[:ssl_ca_file]
           else
-            @connection.ca_file = '../../../conf/cacert.pem'
+            @connection.ca_file = File.dirname(__FILE__) + '/cacert.pem'
           end
         else
           @connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
