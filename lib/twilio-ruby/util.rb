7588f952220f9fe6af39531ca0ca2ddb9bf64645
Chris Barton <cbarton@inigral.com>
2012-04-05 14:14:13 -0700

    Adding in the <= and >= to the query string for date ranges

--- lib/twilio-ruby/util.rb
+++ lib/twilio-ruby/util.rb
@@ -1,7 +1,13 @@
 module Twilio
   module Util
     def url_encode(hash)
-      hash.to_a.map {|p| p.map {|e| CGI.escape e.to_s}.join '='}.join '&'
+      hash.map do |key, param|
+        join_sign = case /^(.*)([<|>]=)$/ =~ key
+          when nil then '='
+          else key = $1; $2
+        end
+        "#{CGI.escape(key.to_s)}#{join_sign}#{CGI.escape(param.to_s)}"
+      end.join '&'
     end
   end
 end
