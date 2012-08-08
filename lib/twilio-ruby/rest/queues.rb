9afcec242bc2dd4435000907972215bb88ece241
Andrew Benton <andrewmbenton@gmail.com>
2012-08-08 00:23:17 -0700

    assign the list key for queues to be queue_members

--- lib/twilio-ruby/rest/queues.rb
+++ lib/twilio-ruby/rest/queues.rb
@@ -1,6 +1,12 @@
 module Twilio
   module REST
-    class Queues < ListResource; end
+    class Queues < ListResource
+      def initialize(uri, client)
+        super
+        # hard-code the json key since 'members' doesn't exist in the response
+        @list_key = 'queue_members'
+      end
+    end
 
     class Queue < InstanceResource
       def initialize(uri, client, params={})
