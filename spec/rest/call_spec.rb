f3e558c7f33abf4961ed655e93ed9011da606202
Geoff Petrie <g.petrie@gmail.com>
2013-01-08 23:34:00 -0700

    WIP: Spec code clean up -- first pass

--- spec/rest/call_spec.rb
+++ spec/rest/call_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
 describe Twilio::REST::Call do
-  it 'should set up a recordings resources object' do
-    call = Twilio::REST::Call.new('someUri', 'someClient')
-    call.respond_to?(:recordings).should == true
-    call.recordings.instance_variable_get('@uri').should == 'someUri/Recordings'
+
+  before do
+    @call = Twilio::REST::Call.new('someUri', 'someClient')
   end
 
-  it 'should set up a notifications resources object' do
-    call = Twilio::REST::Call.new('someUri', 'someClient')
-    call.respond_to?(:notifications).should == true
-    call.notifications.instance_variable_get('@uri').should == 'someUri/Notifications'
+  it 'sets up a recordings resources object' do
+    @call.respond_to?(:recordings).should == true
+    @call.recordings.instance_variable_get('@uri').should == 'someUri/Recordings'
+  end
+
+  it 'sets up a notifications resources object' do
+    @call.respond_to?(:notifications).should == true
+    @call.notifications.instance_variable_get('@uri').should == 'someUri/Notifications'
   end
 end
