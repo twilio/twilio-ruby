5822575608f689bc0de9712b7ac5120351b80479
K Gautam Pai <gautampai@gautampai.com>
2013-03-13 12:52:29 +0530

    Use built-in respond_to matcher

--- spec/rest/account_spec.rb
+++ spec/rest/account_spec.rb
@@ -7,52 +7,52 @@ describe Twilio::REST::Account do
   end
 
   it 'sets up incoming phone numbers resources object' do
-    @account.respond_to?(:incoming_phone_numbers).should == true
+    @account.should respond_to(:incoming_phone_numbers)
     @account.incoming_phone_numbers.instance_variable_get('@uri').should == 'someUri/IncomingPhoneNumbers'
   end
 
   it 'sets up an available phone numbers resources object' do
-    @account.respond_to?(:available_phone_numbers).should == true
+    @account.should respond_to(:available_phone_numbers)
     @account.available_phone_numbers.instance_variable_get('@uri').should == 'someUri/AvailablePhoneNumbers'
   end
 
   it 'sets up an outgoing caller ids resources object' do
-    @account.respond_to?(:outgoing_caller_ids).should == true
+    @account.should respond_to(:outgoing_caller_ids)
     @account.outgoing_caller_ids.instance_variable_get('@uri').should == 'someUri/OutgoingCallerIds'
   end
 
   it 'sets up a calls resources object' do
-    @account.respond_to?(:calls).should == true
+    @account.should respond_to(:calls)
     @account.calls.instance_variable_get('@uri').should == 'someUri/Calls'
   end
 
   it 'sets up a conferences resources object' do
-    @account.respond_to?(:conferences).should == true
+    @account.should respond_to(:conferences)
     @account.conferences.instance_variable_get('@uri').should == 'someUri/Conferences'
   end
 
   it 'sets up a queues resources object' do
-    @account.respond_to?(:queues).should == true
+    @account.should respond_to(:queues)
     @account.queues.instance_variable_get('@uri').should == 'someUri/Queues'
   end
 
   it 'sets up a sms resource object' do
-    @account.respond_to?(:sms).should == true
+    @account.should respond_to(:sms)
     @account.sms.instance_variable_get('@uri').should == 'someUri/SMS'
   end
 
   it 'sets up a recordings resources object' do
-    @account.respond_to?(:recordings).should == true
+    @account.should respond_to(:recordings)
     @account.recordings.instance_variable_get('@uri').should == 'someUri/Recordings'
   end
 
   it 'sets up a transcriptions resources object' do
-    @account.respond_to?(:transcriptions).should == true
+    @account.should respond_to(:transcriptions)
     @account.transcriptions.instance_variable_get('@uri').should == 'someUri/Transcriptions'
   end
 
   it 'sets up a notifications resources object' do
-    @account.respond_to?(:notifications).should == true
+    @account.should respond_to(:notifications)
     @account.notifications.instance_variable_get('@uri').should == 'someUri/Notifications'
   end
 end
--- spec/rest/queue_spec.rb
+++ spec/rest/queue_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Twilio::REST::Queue do
   it 'should set up a members resources object' do
     queue = Twilio::REST::Queue.new('someUri', 'someClient')
-    queue.respond_to?(:members).should == true
+    queue.should respond_to(:members)
     queue.members.instance_variable_get('@uri').should == 'someUri/Members'
   end
 end
