9d44749e7b47a246cdfa0eb13727c15e94a33e28
Doug Black <dblack@twilio.com>
2013-09-06 11:33:54 -0700

    remove Images

--- lib/twilio-ruby/rest/media.rb
+++ lib/twilio-ruby/rest/media.rb
@@ -2,17 +2,11 @@ module Twilio
   module REST
     class Media < ListResource
 
-      SUBRESOURCES = [:images]
-
       def initialize(path, client)
         super
         @list_key = 'media'
       end
 
-      def method_missing(method, *args)
-        super unless SUBRESOURCES.include? method
-        self.class.new "#{@path}/#{twilify(method)}", @client
-      end
     end
 
     class MediaInstance < InstanceResource; end
--- lib/twilio-ruby/rest/media/images.rb
+++
@@ -1,6 +0,0 @@
-module Twilio
-  module REST
-    class Images < ListResource; end
-    class Image < InstanceResource; end
-  end
-end
