0971cd4c27687152e0207ae84d5c805c25fac0b2
Doug Black <dblack@twilio.com>
2013-09-24 20:49:49 -0700

    route sms.messages to .messages

--- lib/twilio-ruby/rest/sms/messages.rb
+++ lib/twilio-ruby/rest/sms/messages.rb
@@ -2,9 +2,8 @@ module Twilio
   module REST
     class Messages < ListResource
       def initialize(path, client)
+        path.sub! '/SMS', ''
         super
-        # hard-code the json key since 'messages' doesn't exist in the response
-        @list_key = 'sms_messages'
       end
     end
 
