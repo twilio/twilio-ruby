510d7a3d50c3ba33410f8df3fe83e56822380145
Doug Black <dblack@twilio.com>
2013-10-17 13:43:33 -0700

    fix indentation

--- lib/twilio-ruby/rest/incoming_phone_numbers/toll_free.rb
+++ lib/twilio-ruby/rest/incoming_phone_numbers/toll_free.rb
@@ -4,8 +4,8 @@ module Twilio
       class TollFree < ListResource
         def initialize(path, client)
           @path, @client = path, client
-            @instance_class = Twilio::REST::IncomingPhoneNumber
-            @list_key, @instance_id_key = 'incoming_phone_numbers', 'sid'
+          @instance_class = Twilio::REST::IncomingPhoneNumber
+          @list_key, @instance_id_key = 'incoming_phone_numbers', 'sid'
         end
       end
     end
