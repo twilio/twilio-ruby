666dfe30091162837b34f0c969737d15137d912d
Kevin Burke <kevin@twilio.com>
2014-02-03 13:54:58 +0100

    Add version to twilio-rubby client

--- lib/twilio-ruby/rest/client.rb
+++ lib/twilio-ruby/rest/client.rb
@@ -50,7 +50,7 @@ module Twilio
       HTTP_HEADERS = {
         'Accept' => 'application/json',
         'Accept-Charset' => 'utf-8',
-        'User-Agent' => "twilio-ruby/#{Twilio::VERSION}",
+        'User-Agent' => "twilio-ruby/#{Twilio::VERSION} (Ruby #{Gem.ruby_version})",
       }
 
       DEFAULTS = {
