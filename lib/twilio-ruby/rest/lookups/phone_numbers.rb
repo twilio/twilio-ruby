54ba72678b13ccbae8a018056c29de3dc01bd2a3
Doug Black <dblack@twilio.com>
2015-03-31 16:26:45 -0700

    fix lookups

--- lib/twilio-ruby/rest/lookups/phone_numbers.rb
+++ lib/twilio-ruby/rest/lookups/phone_numbers.rb
@@ -1,7 +1,16 @@
 module Twilio
   module REST
     module Lookups
-      class PhoneNumbers < Twilio::REST::NextGenListResource; end
+      class PhoneNumbers < Twilio::REST::NextGenListResource;
+        include Twilio::Util
+        include Twilio::REST::Utils
+
+        def get(number, query={})
+          full_path = "#{@path}/#{number}"
+          full_path << "?#{url_encode(twilify(query))}" if !query.empty?
+          @instance_class.new full_path, @client
+        end
+      end
       class PhoneNumber < InstanceResource; end
     end
   end
