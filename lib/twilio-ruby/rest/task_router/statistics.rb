151ed760d83c2ed669fb6bd054ed8ac6f07c152e
Phil Nash <philnash@gmail.com>
2015-04-03 14:12:07 +0100

    Corrects class generation for Ruby 1.9.3

--- lib/twilio-ruby/rest/task_router/statistics.rb
+++ lib/twilio-ruby/rest/task_router/statistics.rb
@@ -5,8 +5,20 @@ module Twilio
         def statistics(args={})
           path = "#{@path}/Statistics"
           response = @client.get(path, args, true)
-          statistic_class = Object.const_get("#{self.class.to_s}Statistics")
-          statistic_class.new(path, @client, response)
+          statistics_class.new(path, @client, response)
+        end
+
+        private
+
+        ##
+        # Gets the class for the statistics of the current class.
+        # Should just be Object.const_get("#{self.class.to_s}Statistics") but
+        # Ruby 1.9.3 did not agree. Can be updated if support is dropped.
+        def statistics_class
+          current_class = self.class.to_s.split('::').last
+          statistics_class = Twilio::REST::TaskRouter.const_get(
+            "#{current_class}Statistics"
+          )
         end
       end
     end
