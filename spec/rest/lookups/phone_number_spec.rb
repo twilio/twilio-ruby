4d7d9c21e45db6f2c3910bbed5ba31069f715025
Phil Nash <philnash@gmail.com>
2015-05-06 01:44:57 +0200

    Adds specs for URI encoding phone numbers

--- spec/rest/lookups/phone_number_spec.rb
+++ spec/rest/lookups/phone_number_spec.rb
@@ -5,4 +5,24 @@ describe Twilio::REST::Lookups::PhoneNumbers do
     client = Twilio::REST::LookupsClient.new 'otherSid', 'otherToken'
     expect(client).to respond_to(:phone_numbers)
   end
+
+  it 'gets phone numbers without special encoding' do
+    number = '+13123131434'
+    client = Twilio::REST::LookupsClient.new 'otherSid', 'otherToken'
+    expect(client).to receive(:get).once
+      .with('/v1/PhoneNumbers/+13123131434')
+      .and_return({ phone_number: number })
+    phone_number = client.phone_numbers.get('+13123131434').phone_number
+    expect(phone_number).to be(number)
+  end
+
+  it 'URI encodes phone number path parameters' do
+    number = '+13123131434'
+    client = Twilio::REST::LookupsClient.new 'otherSid', 'otherToken'
+    expect(client).to receive(:get).once
+      .with('/v1/PhoneNumbers/+1%20312%20313%201434')
+      .and_return({ phone_number: number })
+    phone_number = client.phone_numbers.get('+1 312 313 1434').phone_number
+    expect(phone_number).to be(number)
+  end
 end
