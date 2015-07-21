83fee7a61b73727ef291fd3ece75a39164c1c82f
matt <matt@twilio.com>
2015-07-20 17:02:38 -0700

    Revert token.rb, yoyo compat for call_feedback.rb

--- lib/twilio-ruby/rest/call_feedback.rb
+++ lib/twilio-ruby/rest/call_feedback.rb
@@ -1,17 +1,10 @@
 module Twilio
   module REST
-    class Feedback < ListResource;
-      ##
-      # Get this feedback object.
-      #
-      # Overridden because GETS to /Feedback
-      # returns an instance, not a list.
+    class Feedback < ListResource
       def get
         @instance_class.new @path, @client
       end
 
-      ##
-      # Creates a new Feedback object.
       def create(params={})
         raise "Can't create feedback without a REST Client" unless @client
         response = @client.post @path, params
