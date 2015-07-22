ee05b000bfd58ee339b3078a3ee1787b2671327b
Justin Witz <jwitz@twilio.com>
2015-07-21 17:20:01 -0700

    Add Workflow Builder

--- examples/taskrouter.examples.rb
+++ examples/taskrouter.examples.rb
@@ -3,9 +3,9 @@ require 'twilio-ruby'
 require 'json'
 
 # taskrouter examples version 1
-@account_sid = 'AC9242e2195ab0681fd413fc3c03b7d7f8'
-@auth_token  = '3706b85b34b688189f9a47bf393752cc'
-@workspace_sid = 'WS0b37cb82ee097132c15cc5295dbe42a1'
+@account_sid = 'YourAccountSid'
+@auth_token  = 'YourAuthToken'
+@workspace_sid = 'YourWorkspaceSid'
 
 # set up a client
 @client = Twilio::REST::TaskRouterClient.new @account_sid, @auth_token, @workspace_sid
@@ -17,9 +17,9 @@ puts @client.workspace.sid + ' with ' + @client.workspace.friendly_name
 end
 
 # create a task
-@workflow_sid = 'WW71cfb6c66ac8be88a5f78b527f66c9aa'
+@workflow_sid = 'YourWorkflowSid'
 @task_attributes = '{"foo":"bar"}'
-@client.workspace.tasks.create(attributes: @task_attributes, workflow_sid: @workflow_sid)
+# @client.workspace.tasks.create(attributes: @task_attributes, workflow_sid: @workflow_sid)
 
 # list all the tasks created
 @client.workspace.tasks.list.each do |task|
@@ -28,6 +28,35 @@ end
 end
 
 # get all the statistics for a workspace
-@workspaceStats = @client.workspace.statistics
-@cumulative = @workspaceStats.cumulative
+@workspace_stats = @client.workspace.statistics
+@cumulative = @workspace_stats.cumulative
 puts 'Avg Task Acceptance Time: ' + @cumulative['avg_task_acceptance_time'].to_s + ' with ' + @cumulative['tasks_created'].to_s + ' tasks created'
+
+# build a workflow
+@support_queue_sid = 'YourSupportQueueSid'
+@marketing_queue_sid = 'YourMarketingQueueSid'
+@default_queue_sid = 'YourDefaultQueue'
+
+@support_target = Twilio::TaskRouter::WorkflowRuleTarget.new @support_queue_sid
+@support_targets = [@support_target]
+@support_rule = Twilio::TaskRouter::WorkflowRule.new 'type == support', @support_targets
+
+@marketing_target = Twilio::TaskRouter::WorkflowRuleTarget.new @marketing_queue_sid
+@marketing_targets = [@marketing_target]
+@marketing_rule = Twilio::TaskRouter::WorkflowRule.new 'type == support', @marketing_targets
+
+@rules = [@support_rule, @marketing_rule]
+@default_target = Twilio::TaskRouter::WorkflowRuleTarget.new @default_queue_sid
+
+@config = Twilio::TaskRouter::WorkflowConfiguration.new @rules, @default_target
+puts @config.to_json
+
+@workflowCreated = @client.workspace.workflows.create(configuration: @config.to_json, friendly_name: 'MyWorkflow2', assignment_callback_url: 'http://example.com')
+@workflowFetched = @client.workspace.workflows.get(@workflowCreated.sid)
+
+@config_string = @workflowFetched.configuration
+@config_obj = Twilio::TaskRouter::WorkflowConfiguration.parse_json(@config_string)
+
+puts @config_string
+puts @config_obj.rules[0].expression
+puts @config_obj.default_target.queue
