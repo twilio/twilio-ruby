befcd99dbf40b4b59b15048945b128ef8a984d0c
Carlos Diaz-Padron <cdpadron@twilio.com>
2015-08-24 14:40:38 -0700

    Add Holodeck to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,18 @@ Bundler.setup
 
 Dir.glob(File.expand_path("../support/**/*.rb", __FILE__), &method(:require))
 
+require_relative './holodeck/holodeck.rb'
+require_relative './holodeck/holodeck_resource.rb'
+require_relative './holodeck/hologram.rb'
+Dir[File.dirname(__FILE__) + "/holodeck/decks/resources/*/**.rb"].each do |file|
+  require file
+end
+
+Dir[File.dirname(__FILE__) + "/holodeck/decks/domains/*.rb"].each do |file|
+  require file
+end
+require_relative './holodeck/decks/twilio_deck.rb'
+
 require 'twilio-ruby'
 require 'rack'
 
@@ -12,4 +24,12 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+def account_sid
+  ENV['ACCOUNT_SID']
+end
+
+def auth_token
+  ENV['AUTH_TOKEN']
 end
