08ca909f8ffe528477c3f00da6c06d048c09e6ad
Jingming Niu <jniu@twilio.com>
2015-11-04 14:36:21 -0800

    First person grants

---
+++ lib/twilio-ruby/util/access_token.rb
@@ -0,0 +1,97 @@
+module Twilio
+  module Util
+    class AccessToken
+      def initialize(signing_key_id, account_sid, secret, ttl=3600, identity=nil)
+        @signing_key_sid = signing_key_id
+        @account_sid = account_sid
+        @secret = secret
+        @ttl = ttl
+        @identity = identity
+        @grants = []
+      end
+
+      def add_grant(grant)
+        @grants.push(grant)
+        self
+      end
+
+      def to_jwt(algorithm='HS256')
+        now = Time.now.to_i - 1
+        headers = {
+            'cty' => 'twilio-sat;v=2',
+            'typ' => 'JWT'
+        }
+
+        grants = {}
+        if @identity
+          grants['identity'] = @identity
+        end
+
+        @grants.each { |grant| grants[grant.key] = grant.payload }
+
+        payload = {
+            'jti' => "#{@signing_key_sid}-#{now}",
+            'iss' => @signing_key_sid,
+            'sub' => @account_sid,
+            'nbf' => now,
+            'exp' => now + @ttl,
+            'grants' => grants
+        }
+        JWT.encode payload, @secret, algorithm, headers
+      end
+
+      def to_s
+        to_jwt
+      end
+
+      class ConversationGrant
+        attr_writer :configuration_profile_sid
+
+        def key
+          'rtc'
+        end
+
+        def payload
+          payload = {}
+          if @configuration_profile_sid
+            payload['configuration_profile_sid'] = @configuration_profile_sid
+          end
+
+          payload
+        end
+
+      end
+
+      class IpMessagingGrant
+        attr_writer :service_sid
+        attr_writer :endpoint_id
+        attr_writer :role_sid
+        attr_writer :credential_sid
+
+        def key
+          'ip_messaging'
+        end
+
+        def payload
+          payload = {}
+          if @service_sid
+            payload['service_sid'] = @service_sid
+          end
+          if @endpoint_id
+            payload['endpoint_id'] = @endpoint_id
+          end
+          if @role_sid
+            payload['deployment_role_sid'] = @role_sid
+          end
+          if @credential_sid
+            payload['push_credential_sid'] = @credential_sid
+          end
+
+          payload
+        end
+
+      end
+
+    end
+  end
+end
---
+++ spec/util/access_token_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+describe Twilio::Util::AccessToken do
+
+  it 'should generate a token for no grants' do
+    scat = Twilio::Util::AccessToken.new 'SK123', 'AC123', 'secret'
+    token = scat.to_s
+    expect(token).not_to be_nil
+    payload, header = JWT.decode token, 'secret'
+
+    expect(payload['iss']).to eq('SK123')
+    expect(payload['sub']).to eq('AC123')
+    expect(payload['nbf']).not_to be_nil
+    expect(payload['exp']).not_to be_nil
+    expect(payload['nbf'] + 3600).to eq(payload['exp'])
+    expect(payload['jti']).not_to be_nil
+    expect("#{payload['iss']}-#{payload['nbf']}").to eq(payload['jti'])
+    expect(payload['grants']).not_to be_nil
+    expect(payload['grants'].count).to eq(0)
+  end
+
+  it 'should be able to add conversation grant' do
+    scat = Twilio::Util::AccessToken.new 'SK123', 'AC123', 'secret'
+    scat.add_grant(Twilio::Util::AccessToken::ConversationGrant.new)
+
+    token = scat.to_s
+    expect(token).not_to be_nil
+    payload, header = JWT.decode token, 'secret'
+
+    expect(payload['iss']).to eq('SK123')
+    expect(payload['sub']).to eq('AC123')
+    expect(payload['nbf']).not_to be_nil
+    expect(payload['exp']).not_to be_nil
+    expect(payload['nbf'] + 3600).to eq(payload['exp'])
+    expect(payload['jti']).not_to be_nil
+    expect("#{payload['iss']}-#{payload['nbf']}").to eq(payload['jti'])
+    expect(payload['grants']).not_to be_nil
+    expect(payload['grants'].count).to eq(1)
+    expect(payload['grants']['rtc']).not_to be_nil
+  end
+
+  it 'should be able to add endpoint grants' do
+    scat = Twilio::Util::AccessToken.new 'SK123', 'AC123', 'secret'
+    scat.add_grant(Twilio::Util::AccessToken::IpMessagingGrant.new)
+
+    token = scat.to_s
+    expect(token).not_to be_nil
+    payload, header = JWT.decode token, 'secret'
+
+    expect(payload['iss']).to eq('SK123')
+    expect(payload['sub']).to eq('AC123')
+    expect(payload['nbf']).not_to be_nil
+    expect(payload['exp']).not_to be_nil
+    expect(payload['nbf'] + 3600).to eq(payload['exp'])
+    expect(payload['jti']).not_to be_nil
+    expect("#{payload['iss']}-#{payload['nbf']}").to eq(payload['jti'])
+    expect(payload['grants']).not_to be_nil
+    expect(payload['grants'].count).to eq(1)
+    expect(payload['grants']['ip_messaging']).not_to be_nil
+  end
+
+  it 'should add rest grants' do
+    scat = Twilio::Util::AccessToken.new 'SK123', 'AC123', 'secret'
+    scat.add_grant(Twilio::Util::AccessToken::ConversationGrant.new)
+    scat.add_grant(Twilio::Util::AccessToken::IpMessagingGrant.new)
+
+    token = scat.to_s
+    expect(token).not_to be_nil
+    payload, header = JWT.decode token, 'secret'
+
+    expect(payload['iss']).to eq('SK123')
+    expect(payload['sub']).to eq('AC123')
+    expect(payload['nbf']).not_to be_nil
+    expect(payload['exp']).not_to be_nil
+    expect(payload['nbf'] + 3600).to eq(payload['exp'])
+    expect(payload['jti']).not_to be_nil
+    expect("#{payload['iss']}-#{payload['nbf']}").to eq(payload['jti'])
+    expect(payload['grants']).not_to be_nil
+    expect(payload['grants'].count).to eq(2)
+    expect(payload['grants']['rtc']).not_to be_nil
+    expect(payload['grants']['ip_messaging']).not_to be_nil
+  end
+
+end
