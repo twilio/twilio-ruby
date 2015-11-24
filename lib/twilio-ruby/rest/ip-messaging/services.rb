bd22fa9924e88654a8f06f0f0f519c7d361b5986
Carlos Diaz-Padron <cdpadron@twilio.com>
2015-11-23 16:37:23 -0800

    Fix IPMessaging services

--- lib/twilio-ruby/rest/ip-messaging/services.rb
+++ lib/twilio-ruby/rest/ip-messaging/services.rb
@@ -1,17 +1,12 @@
 module Twilio
   module REST
     module IpMessaging
-      class Services < NextGenListResource
-        def initialize(path, client)
-          @path, @client = path, client
-          @submodule = :IpMessaging
-        end
-      end
+      class Services < NextGenListResource; end
 
       class Service < InstanceResource
         def initialize(path, client, params={})
           super
-          @submodule = :IpMessaging
+
           resource :channels,
                    :users,
                    :roles
