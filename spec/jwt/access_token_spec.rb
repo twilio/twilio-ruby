6c816c5853ac92ab9d6791994c535a7d9da9aac7
Jingming Niu <niu@jingming.ca>
2016-08-07 22:29:48 -0700

    Add programmable voice grant

--- spec/jwt/access_token_spec.rb
+++ spec/jwt/access_token_spec.rb
@@ -111,4 +111,29 @@ describe Twilio::JWT::AccessToken do
     expect(payload['grants']['ip_messaging']).not_to be_nil
   end
 
+  it 'should add programmable voice grant' do
+    scat = Twilio::JWT::AccessToken.new 'AC123', 'SK123','secret'
+    pvg = Twilio::JWT::AccessToken::ProgrammableVoiceGrant.new
+    pvg.outgoing_application_sid = 'AP123'
+    pvg.outgoing_application_params = { :foo => 'bar' }
+
+    scat.add_grant(pvg)
+
+    token = scat.to_s
+    expect(token).not_to be_nil
+    payload, header = JWT.decode token, 'secret'
+
+    expect(payload['iss']).to eq('SK123')
+    expect(payload['sub']).to eq('AC123')
+    expect(payload['exp']).not_to be_nil
+    expect(payload['exp']).to be >= Time.now.to_i
+    expect(payload['jti']).not_to be_nil
+    expect(payload['jti']).to start_with payload['iss']
+    expect(payload['grants']).not_to be_nil
+    expect(payload['grants'].count).to eq(1)
+    expect(payload['grants']['programmable_voice']).not_to be_nil
+    expect(payload['grants']['programmable_voice']['outgoing']['application_sid']).to eq('AP123')
+    expect(payload['grants']['programmable_voice']['outgoing']['params']['foo']).to eq('bar')
+  end
+
 end
