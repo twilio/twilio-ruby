1eae39f20cc4a4c9d702d022e6f3df77735cb544
Evan Fossier <evan.fossier@gmail.com>
2017-10-20 12:54:24 -0700

    Correctly set headers on Twilio::Response (#359)

--- lib/twilio-ruby/framework/response.rb
+++ lib/twilio-ruby/framework/response.rb
@@ -1,11 +1,12 @@
 module Twilio
   class Response
-    attr_accessor :status_code, :body
+    attr_accessor :status_code, :body, :headers
 
-    def initialize(status_code, body)
+    def initialize(status_code, body, headers: nil)
       @status_code = status_code
       body = '{}' if !body || body.empty?
       @body = JSON.parse(body)
+      @headers = !headers ? {} : headers.to_hash
     end
 
     def to_s
