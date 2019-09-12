0c05c0565dc5308121ef71250ca309ccef33e284
childish-sambino <sharrison@twilio.com>
2019-09-12 11:31:02 -0500

    Add a global configuration for the HTTP client (#467)

--- spec/rest/client_spec.rb
+++ spec/rest/client_spec.rb
@@ -49,6 +49,28 @@ describe Twilio::REST::TrunkingClient do
 end
 
 describe Twilio::REST::Client do
+  before do
+    Twilio.configure do |config|
+      config.account_sid = 'someSid'
+      config.auth_token = 'someToken'
+      config.http_client = 'someClient'
+    end
+  end
+
+  it 'uses the global configuration by default' do
+    @client = Twilio::REST::Client.new
+    expect(@client.account_sid).to eq('someSid')
+    expect(@client.auth_token).to eq('someToken')
+    expect(@client.http_client).to eq('someClient')
+  end
+
+  it 'uses the arguments over global configuration' do
+    @client = Twilio::REST::Client.new('myUser', 'myPassword', nil, nil, 'myClient')
+    expect(@client.account_sid).to eq('myUser')
+    expect(@client.auth_token).to eq('myPassword')
+    expect(@client.http_client).to eq('myClient')
+  end
+
   it 'successfully validates the working SSL certificate' do
     @holodeck.mock Twilio::Response.new(200, '')
     expect { @client.validate_ssl_certificate }.not_to raise_error
--- spec/twilio_spec.rb
+++ spec/twilio_spec.rb
@@ -3,13 +3,15 @@ describe Twilio do
     Twilio.instance_variable_set('@configuration', nil)
   end
 
-  it 'should set the account sid and auth token with a config block' do
+  it 'should set the configuration with a config block' do
     Twilio.configure do |config|
       config.account_sid = 'someSid'
       config.auth_token = 'someToken'
+      config.http_client = 'someClient'
     end
 
     expect(Twilio.account_sid).to eq('someSid')
     expect(Twilio.auth_token).to eq('someToken')
+    expect(Twilio.http_client).to eq('someClient')
   end
 end
