3f49490b18665edc2897607e4ed971bcc3ed0562
cchua <cchua@twilio.com>
2022-05-02 13:10:56 -0600

    include dash in osName regrex exp

--- spec/rest/client_spec.rb
+++ spec/rest/client_spec.rb
@@ -263,12 +263,12 @@ describe Twilio::REST::Client do
 
     it 'use default user agent format' do
       @client.request('host', 'port', 'GET', 'https://api.twilio.com')
-      expect(@client.http_client.last_request.headers['User-Agent']).to match %r{^twilio-ruby/[0-9.]+\s\(\w+\s\w+\)\sRuby/[^\s]+$}
+      expect(@client.http_client.last_request.headers['User-Agent']).to match %r{^twilio-ruby/[0-9.]+\s\([\w+-]\s\w+\)\sRuby/[^\s]+$}
     end
 
     it 'add user agent extensions' do
       extensions = ['twilio-run/2.0.0-test', 'flex-plugin/3.4.0']
-      @client.user_agent_extensions = extensions
+      @client.user_agent_extensions = extensionsgit 
       @client.request('host', 'port', 'GET', 'https://api.twilio.com')
       actual_extensions = @client.http_client.last_request.headers['User-Agent'].split(/ /).last(extensions.size)
       expect(actual_extensions).to match_array(extensions)
