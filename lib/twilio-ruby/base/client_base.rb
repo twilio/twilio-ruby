4dc58c3daeeed86eae8bf1fa0b462d825c1bb454
sburman <sburman@twilio.com>
2022-12-05 14:35:33 +0530

    rubocop saga

--- lib/twilio-ruby/base/client_base.rb
+++ lib/twilio-ruby/base/client_base.rb
@@ -1,41 +1,38 @@
 module Twilio
   module REST
     class ClientBase
+      # rubocop:disable Style/ClassVars
       @@default_region = 'us1'
 
-      attr_accessor :http_client, :username, :password, :account_sid, :auth_token, :region, :edge, :logger, :user_agent_extensions
+      # rubocop:enable Style/ClassVars
+      attr_accessor :http_client, :username, :password, :account_sid, :auth_token, :region, :edge, :logger,
+                    :user_agent_extensions
 
-      
       ##
       # Makes a request to the Twilio API using the configured http client
       # Authentication information is automatically added if none is provided
-      def request(host, port, method, uri, params={}, data={}, headers={}, auth=nil, timeout=nil)
+      def request(host, port, method, uri, params = {}, data = {}, headers = {}, auth = nil, timeout = nil) # rubocop:disable Metrics/MethodLength
         auth ||= @auth
 
         ruby_config = RbConfig::CONFIG
-        headers['User-Agent'] = "twilio-ruby/#{Twilio::VERSION} (#{ruby_config["host_os"]} #{ruby_config["host_cpu"]}) Ruby/#{RUBY_VERSION}"
+        headers['User-Agent'] =
+          "twilio-ruby/#{Twilio::VERSION} (#{ruby_config['host_os']} #{ruby_config['host_cpu']}) Ruby/#{RUBY_VERSION}"
         headers['Accept-Charset'] = 'utf-8'
 
         user_agent_extensions.each { |extension| headers['User-Agent'] += " #{extension}" }
 
-        if method == 'POST' && !headers['Content-Type']
-          headers['Content-Type'] = 'application/x-www-form-urlencoded'
-        end
+        headers['Content-Type'] = 'application/x-www-form-urlencoded' if method == 'POST' && !headers['Content-Type']
 
-        unless headers['Accept']
-          headers['Accept'] = 'application/json'
-        end
+        headers['Accept'] = 'application/json' unless headers['Accept']
 
         uri = build_uri(uri)
 
         if @logger
-          @logger.debug("--BEGIN Twilio API Request--")
+          @logger.debug('--BEGIN Twilio API Request--')
           @logger.debug("Request Method: <#{method}>")
 
           headers.each do |key, value|
-            unless key.downcase == 'authorization'
-              @logger.debug("#{key}:#{value}")
-            end
+            @logger.debug("#{key}:#{value}") unless key.downcase == 'authorization'
           end
 
           url = URI(uri)
@@ -60,7 +57,7 @@ module Twilio
         if @logger
           @logger.debug("Response Status Code:#{response.status_code}")
           @logger.debug("Response Headers:#{response.headers}")
-          @logger.debug("--END TWILIO API REQUEST--")
+          @logger.debug('--END TWILIO API REQUEST--')
         end
 
         response
@@ -69,9 +66,7 @@ module Twilio
       ##
       # Build the final request uri
       def build_uri(uri)
-        if @region.nil? and @edge.nil?
-          return uri
-        end
+        return uri if @region.nil? && @edge.nil?
 
         parsed_url = URI(uri)
         pieces = parsed_url.host.split('.')
@@ -80,18 +75,17 @@ module Twilio
         new_edge = @edge
         new_region = @region
 
-        if pieces.length == 4
+        case pieces.length
+        when 4
           new_region ||= pieces[1]
-        elsif pieces.length == 5
+        when 5
           new_edge ||= pieces[1]
           new_region ||= pieces[2]
         end
 
-        if !new_edge.nil? && new_region.nil?
-          new_region = @@default_region
-        end
+        new_region = @@default_region if !new_edge.nil? && new_region.nil?
 
-        parsed_url.host = [product, new_edge, new_region, domain].select {|item| !item.nil?}.join('.')
+        parsed_url.host = [product, new_edge, new_region, domain].reject(&:nil?).join('.')
         parsed_url.to_s
       end
 
@@ -99,9 +93,9 @@ module Twilio
       # Validate the new SSL certificate for the Twilio API
       def validate_ssl_certificate
         response = request('api.twilio.com', '8443', 'GET', 'https://api.twilio.com:8443/.json')
-        if response.status_code < 200 || response.status_code >= 300
-          raise RestError.new 'Unexpected response from certificate endpoint', response
-        end
+        return unless response.status_code < 200 || response.status_code >= 300
+
+        raise RestError.new 'Unexpected response from certificate endpoint', response
       end
     end
   end
