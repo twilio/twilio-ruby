0230689d28843308a41270b4815167ccacbd9863
James Yorston <jamesyorston1994@gmail.com>
2023-06-20 07:41:32 +0100

    Fix local jump error due to lack of block passing for recording list (#665)

--- lib/twilio-ruby/rest/video/v1/recording.rb
+++ lib/twilio-ruby/rest/video/v1/recording.rb
@@ -126,7 +126,7 @@ module Twilio
                             
                             'SourceSid' => source_sid,
                             
-                            'GroupingSid' =>  Twilio.serialize_list(grouping_sid),
+                            'GroupingSid' =>  Twilio.serialize_list(grouping_sid) { |e| e },
                             
                             'DateCreatedAfter' =>  Twilio.serialize_iso8601_datetime(date_created_after),
                             
