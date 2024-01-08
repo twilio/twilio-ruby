928f02a90409017e873ce45131de64730266f1f3
Miriam Grossi <45015455+miriamela@users.noreply.github.com>
2024-01-08 11:08:58 +0100

    chore: Removing tests related to Autopilot and Understand Endpoints - product EoL (#691)
    * chore: Removing tests related to Autopilot and Understand Endpoints - product EoL
    
    * VINtEL-1587 re-instating Insights in client.rb

--- lib/twilio-ruby/rest/client.rb
+++ lib/twilio-ruby/rest/client.rb
@@ -27,11 +27,6 @@ module Twilio
                 @api ||= Api.new self
             end
             ##
-            # Access the Autopilot Twilio Domain
-            def autopilot
-                @autopilot ||= Autopilot.new self
-            end
-            ##
             # Access the Bulkexports Twilio Domain
             def bulkexports
                 @bulkexports ||= Bulkexports.new self
@@ -66,7 +61,6 @@ module Twilio
             def frontline_api
                 @frontline_api ||= FrontlineApi.new self
             end
-            ##
             # Access the Insights Twilio Domain
             def insights
                 @insights ||= Insights.new self
@@ -224,7 +218,7 @@ module Twilio
             def available_phone_numbers(countryCode=:unset)
                 self.api.v2010.account.available_phone_numbers(countryCode)
             end
-            
+
             ##
             # @param [string] sid The SID of the Call resource to fetch.
 
@@ -273,8 +267,8 @@ module Twilio
             def messages(sid=:unset)
                 self.api.v2010.account.messages(sid)
             end
-            
-            
+
+
             ##
             # @param [string] sid The Twilio-provided string that uniquely identifies the Notification resource to fetch.
 
@@ -322,8 +316,8 @@ module Twilio
             def signing_keys(sid=:unset)
                 self.api.v2010.account.signing_keys(sid)
             end
-            
-            
+
+
             ##
             # @param [string] sid The Twilio-provided string that uniquely identifies the Transcription resource to fetch.
 
@@ -332,8 +326,8 @@ module Twilio
             def transcriptions(sid=:unset)
                 self.api.v2010.account.transcriptions(sid)
             end
-            
-            
+
+
 
             ##
             # Provide a user friendly representation
