3e85e231ec5710c933432dac73649ae01ce23864
Shubham Tiwari <stiwari@twilio.com>
2024-01-17 11:07:34 +0530

    chore: add domain detail

--- lib/twilio-ruby/rest/client.rb
+++ lib/twilio-ruby/rest/client.rb
@@ -117,6 +117,11 @@ module Twilio
                 @preview ||= Preview.new self
             end
             ##
+            # Access the Preview messaging Twilio Domain
+            def preview_messaging
+                @preview_messaging ||= PreviewMessaging.new self
+            end
+            ##
             # Access the Pricing Twilio Domain
             def pricing
                 @pricing ||= Pricing.new self
@@ -219,7 +224,7 @@ module Twilio
             def available_phone_numbers(countryCode=:unset)
                 self.api.v2010.account.available_phone_numbers(countryCode)
             end
-            
+
             ##
             # @param [string] sid The SID of the Call resource to fetch.
 
@@ -268,8 +273,8 @@ module Twilio
             def messages(sid=:unset)
                 self.api.v2010.account.messages(sid)
             end
-            
-            
+
+
             ##
             # @param [string] sid The Twilio-provided string that uniquely identifies the Notification resource to fetch.
 
@@ -317,8 +322,8 @@ module Twilio
             def signing_keys(sid=:unset)
                 self.api.v2010.account.signing_keys(sid)
             end
-            
-            
+
+
             ##
             # @param [string] sid The Twilio-provided string that uniquely identifies the Transcription resource to fetch.
 
@@ -327,8 +332,8 @@ module Twilio
             def transcriptions(sid=:unset)
                 self.api.v2010.account.transcriptions(sid)
             end
-            
-            
+
+
 
             ##
             # Provide a user friendly representation
