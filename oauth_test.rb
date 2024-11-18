ae7271a55ebc31f50af68a20378177f27021d7a8
manisha1997 <singhmanisha.250025@gmail.com>
2024-11-18 11:46:46 +0530

    feat: add oauth support for ruby

--- oauth_test.rb
+++ oauth_test.rb
@@ -1,8 +1,8 @@
 require './lib/twilio-ruby'
 require './lib/twilio-ruby/base/client_base'
 require './lib/twilio-ruby/credential/orgs_credential_provider'
-credentialProvider = Twilio::REST::OrgsCredentialProvider.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
-client = Twilio::REST::Client.new.credential_provider(credentialProvider)
+credential_provider = Twilio::REST::OrgsCredentialProvider.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
+client = Twilio::REST::Client.new.credential_provider(credential_provider)
 response = client.preview_iam.organizations(ENV['ORG_SID'])
                  .accounts('').fetch
 # client_2 = Twilio::REST::Client.new(ENV['SID'], ENV['TOKEN'])
