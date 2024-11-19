5f42ab8a71bbc7bf602aefc6ef02e8c90cfab30f
manisha1997 <singhmanisha.250025@gmail.com>
2024-11-19 12:35:54 +0530

    chore: add messages example for oauth

--- oauth_test.rb
+++ oauth_test.rb
@@ -2,9 +2,9 @@ require './lib/twilio-ruby'
 require './lib/twilio-ruby/base/client_base'
 require './lib/twilio-ruby/credential/orgs_credential_provider'
 credential_provider = Twilio::REST::OrgsCredentialProvider.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
-client = Twilio::REST::Client.new.credential_provider(credential_provider)
-response = client.preview_iam.organizations(ENV['ORG_SID'])
-                 .accounts('').fetch
+client = Twilio::REST::Client.new(ENV['SID']).credential_provider(credential_provider)
+# response = client.preview_iam.organizations(ENV['ORG_SID'])
+#                  .accounts('').fetch
 # client_2 = Twilio::REST::Client.new(ENV['SID'], ENV['TOKEN'])
-# response = client_2.messages.create(from: ENV['FROM'], to: ENV['TO'], body: 'Hello World')
+response = client.messages.create(from: ENV['FROM'], to: ENV['TO'], body: 'Hello World')
 puts response
