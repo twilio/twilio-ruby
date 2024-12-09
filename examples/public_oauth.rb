5a1ff9d7413df27957f7eee2eb21f9104167ac42
manisha1997 <singhmanisha.250025@gmail.com>
2024-12-09 16:15:12 +0530

    feat: add oauth support for ruby

--- examples/public_oauth.rb
+++ examples/public_oauth.rb
@@ -2,7 +2,7 @@ require 'twilio-ruby'
 require 'twilio-ruby/credential/client_credential_provider'
 
 credential_provider = Twilio::REST::ClientCredentialProvider.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
-client = Twilio::REST::Client.new(ENV['ACCOUNT_SID']).credential_provider(credential_provider)
+client = Twilio::REST::Client.new(ENV['ACCOUNT_SID']).credential_provider(credential_provider) # passing account sid is not mandatory
 
 # send messages
 client.messages.create(
