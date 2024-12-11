c6dce6acc5f61ad736c2cf369976b47d6292df5a
manisha1997 <singhmanisha.250025@gmail.com>
2024-12-11 11:45:53 +0530

    feat: add oauth support for ruby

--- cluster/cluster_oauth_spec.rb
+++ cluster/cluster_oauth_spec.rb
@@ -5,8 +5,8 @@ require './lib/twilio-ruby/credential/client_credential_provider'
 describe 'Cluster Test' do
   before(:each) do
     @account_sid = ENV['TWILIO_ACCOUNT_SID']
-    @client_secret = ENV['TWILIO_OAUTH_CLIENT_SECRET']
-    @client_id = ENV['TWILIO_OAUTH_CLIENT_ID']
+    @client_secret = ENV['TWILIO_CLIENT_SECRET']
+    @client_id = ENV['TWILIO_CLIENT_ID']
     @message_sid = ENV['TWILIO_MESSAGE_SID']
     @credential = Twilio::REST::ClientCredentialProvider.new(@client_id, @client_secret)
     @client = Twilio::REST::Client.new(@account_sid).credential_provider(@credential)
