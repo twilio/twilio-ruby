a3113a654a7b0134f7d88cb2236e4991ccb716ed
Manisha Singh <singhmanisha.250025@gmail.com>
2025-05-14 20:03:35 +0530

    chore: move from preview_iam to iam token endpoint (#749)
    * chore: move from preview_iam to iam token endpoint
    
    * chore: move from preview_iam to iam token endpoint

--- lib/twilio-ruby/http/client_token_manager.rb
+++ lib/twilio-ruby/http/client_token_manager.rb
@@ -19,8 +19,8 @@ module Twilio
 
       def fetch_access_token
         client = Twilio::REST::Client.new
-        token_instance = client.preview_iam.v1.token.create(grant_type: @grant_type,
-                                                            client_id: @client_id, client_secret: @client_secret)
+        token_instance = client.iam.v1.token.create(grant_type: @grant_type,
+                                                    client_id: @client_id, client_secret: @client_secret)
         token_instance.access_token
       end
     end
