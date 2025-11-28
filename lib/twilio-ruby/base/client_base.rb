87a98e697a8ad7544460446010b04e6c1e390b34
manisha1997 <singhmanisha.250025@gmail.com>
2025-11-28 15:23:44 +0530

    fix: Regional API domain processing

--- lib/twilio-ruby/base/client_base.rb
+++ lib/twilio-ruby/base/client_base.rb
@@ -102,7 +102,9 @@ module Twilio
       # Build the final request uri
       def build_uri(uri)
         if @edge.nil? && @region && @@region_mappings[@region]
+          ## rubocop:disable Layout/LineLength
           warn '[DEPRECATION] Setting default `Edge` for the provided `region`. For regional processing,DNS is of format product.<city>.<region>.twilio.com; otherwise use product.twilio.com.'
+          # rubocop:enable Layout/LineLength
           @edge = @@region_mappings[@region]
         end
         return uri if @region.nil? && @edge.nil?
