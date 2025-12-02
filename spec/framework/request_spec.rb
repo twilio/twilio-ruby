79326fb8a988f3330418bb2f1df5705c3cf2ded4
manisha1997 <singhmanisha.250025@gmail.com>
2025-12-02 16:50:10 +0530

    chore: URL query percent encoded

--- spec/framework/request_spec.rb
+++ spec/framework/request_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe Twilio::Request do
   it 'should initialize readers correctly' do
     request = Twilio::Request.new('host',
-                                   'port',
-                                   'POST',
-                                   'url',
-                                   { 'param-key' => 'param-value', 'param-keytwo' => 'param-valuetwo' },
-                                   { 'data-key' => 'data-value', 'data-keytwo' => 'data-valuetwo' },
-                                   { 'header-key' => 'header-value', 'header-keytwo' => 'header-valuetwo' },
-                                   ['a', 'b'],
-                                   'timeout')
+                                  'port',
+                                  'POST',
+                                  'url',
+                                  { 'param-key' => 'param-value', 'param-keytwo' => 'param-valuetwo' },
+                                  { 'data-key' => 'data-value', 'data-keytwo' => 'data-valuetwo' },
+                                  { 'header-key' => 'header-value', 'header-keytwo' => 'header-valuetwo' },
+                                  ['a', 'b'],
+                                  'timeout')
     expect(request.host).to eq('host')
     expect(request.port).to eq('port')
     expect(request.method).to eq('POST')
@@ -24,14 +24,14 @@ describe Twilio::Request do
 
   it 'should be represented correctly' do
     request = Twilio::Request.new('host',
-                                   'port',
-                                   'POST',
-                                   'url',
-                                   { 'param-key' => 'param-value', 'param-keytwo' => 'param-valuetwo' },
-                                   { 'data-key' => 'data-value', 'data-keytwo' => 'data-valuetwo' },
-                                   { 'header-key' => 'header-value', 'header-keytwo' => 'header-valuetwo' },
-                                   ['a', 'b'],
-                                   'timeout')
+                                  'port',
+                                  'POST',
+                                  'url',
+                                  { 'param-key' => 'param-value', 'param-keytwo' => 'param-valuetwo' },
+                                  { 'data-key' => 'data-value', 'data-keytwo' => 'data-valuetwo' },
+                                  { 'header-key' => 'header-value', 'header-keytwo' => 'header-valuetwo' },
+                                  ['a', 'b'],
+                                  'timeout')
     expected_string = %((a,b) POST url?param-key=param-value&param-keytwo=param-valuetwo) +
                       %(\n-d "data-key"="data-value") +
                       %(\n-d "data-keytwo"="data-valuetwo") +
@@ -51,10 +51,10 @@ describe Twilio::Request do
                                   ['a', 'b'],
                                   'timeout')
     expected_string = %((a,b) POST url?param-key=%3A%2F%3F%23%5B%5D%40) +
-      %(\n-d "data-key"="data-value") +
-      %(\n-d "data-keytwo"="data-valuetwo") +
-      %(\n-H "header-key": "header-value") +
-      %(\n-H "header-keytwo": "header-valuetwo")
+                      %(\n-d "data-key"="data-value") +
+                      %(\n-d "data-keytwo"="data-valuetwo") +
+                      %(\n-H "header-key": "header-value") +
+                      %(\n-H "header-keytwo": "header-valuetwo")
     expect(request.to_s).to eq(expected_string)
   end
 
@@ -69,10 +69,10 @@ describe Twilio::Request do
                                   ['a', 'b'],
                                   'timeout')
     expected_string = %((a,b) POST url?param-key=%21%24%26%27%28%29%2A%2B%2C%3B%3D) +
-      %(\n-d "data-key"="data-value") +
-      %(\n-d "data-keytwo"="data-valuetwo") +
-      %(\n-H "header-key": "header-value") +
-      %(\n-H "header-keytwo": "header-valuetwo")
+                      %(\n-d "data-key"="data-value") +
+                      %(\n-d "data-keytwo"="data-valuetwo") +
+                      %(\n-H "header-key": "header-value") +
+                      %(\n-H "header-keytwo": "header-valuetwo")
     expect(request.to_s).to eq(expected_string)
   end
 
@@ -87,10 +87,10 @@ describe Twilio::Request do
                                   ['a', 'b'],
                                   'timeout')
     expected_string = %((a,b) POST url?param-key=%2525) +
-      %(\n-d "data-key"="data-value") +
-      %(\n-d "data-keytwo"="data-valuetwo") +
-      %(\n-H "header-key": "header-value") +
-      %(\n-H "header-keytwo": "header-valuetwo")
+                      %(\n-d "data-key"="data-value") +
+                      %(\n-d "data-keytwo"="data-valuetwo") +
+                      %(\n-H "header-key": "header-value") +
+                      %(\n-H "header-keytwo": "header-valuetwo")
     expect(request.to_s).to eq(expected_string)
   end
 
@@ -105,10 +105,10 @@ describe Twilio::Request do
                                   ['a', 'b'],
                                   'timeout')
     expected_string = %((a,b) POST url?param-key=-._~) +
-      %(\n-d "data-key"="data-value") +
-      %(\n-d "data-keytwo"="data-valuetwo") +
-      %(\n-H "header-key": "header-value") +
-      %(\n-H "header-keytwo": "header-valuetwo")
+                      %(\n-d "data-key"="data-value") +
+                      %(\n-d "data-keytwo"="data-valuetwo") +
+                      %(\n-H "header-key": "header-value") +
+                      %(\n-H "header-keytwo": "header-valuetwo")
     expect(request.to_s).to eq(expected_string)
   end
 
@@ -123,10 +123,10 @@ describe Twilio::Request do
                                   ['a', 'b'],
                                   'timeout')
     expected_string = %((a,b) POST url?param-key=https%3A%2F%2Fmalicious.com%2F%3Fq%3D%3Cscript%3Ealert%28%22xss%22%29%3C%2Fscript%3E) +
-      %(\n-d "data-key"="data-value") +
-      %(\n-d "data-keytwo"="data-valuetwo") +
-      %(\n-H "header-key": "header-value") +
-      %(\n-H "header-keytwo": "header-valuetwo")
+                      %(\n-d "data-key"="data-value") +
+                      %(\n-d "data-keytwo"="data-valuetwo") +
+                      %(\n-H "header-key": "header-value") +
+                      %(\n-H "header-keytwo": "header-valuetwo")
     expect(request.to_s).to eq(expected_string)
   end
 end
